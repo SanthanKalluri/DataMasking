b820bb178a59e7e9c6324441a6a2ceb4c78a6ff2
Santhan <santhan.kalluri@gmail.com>
2018-03-13 05:27:46 +0530

    code checkin

---
+++ get_enc_val.sql
@@ -0,0 +1,4 @@
+create or replace unction get_enc_val(i_in in varchar2, i_mode varchar2:='FULL', i_delimiter varchar2:=null, i_occurance number:=1) return varchar2
+IS
+BEGIN
+END;
---
+++ sql_queries.sql
@@ -0,0 +1,3 @@
+create table mask_map(key vrchar2(30), value varchar2(30));
+insert into mask_map values('0123456789','987654321');
+insert into mask_map values('ABCDEFGHIJKLMNOPQRSTUVWXYZ','ZYXWVUTSRQPONMLKJIHGFEDCBA');
